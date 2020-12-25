15b838b89818cec8d1e7be95033b9ab332c48968
SamJay0 <samjay0.254@gmail.com>
2020-12-25 12:49:24 +0300

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hi, am Juma the Developer';
+document.querySelector('button').onclick = function() {
+    alert('You just clicked the button');
+}
