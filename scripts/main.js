5d3c6c1d116a9cc14c13e609e6f7eafbff0cd03c
SamJay0 <samjay0.254@gmail.com>
2020-12-25 13:37:45 +0300

    javascript variables and functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,16 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hi, am Juma the Developer';
+// variables and functions in use
+let myImage = document.querySelector('img')
+myImage.onclick = function() {
+   let imgSrc = myImage.getAttribute('src');
+   if(imgSrc === "images/0.PNG"){
+       myImage.setAttribute('src', 'images/1.PNG');
+   }else {
+    myImage.setAttribute('src', 'images/0.PNG');
+   }
+}
 document.querySelector('button').onclick = function() {
     alert('You just clicked the button');
+
 }
